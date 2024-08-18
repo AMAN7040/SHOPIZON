5c98318d301383bcd021e98da6bd35539168da39
AMAN7040 <amansingh79620@gmail.com>
2024-08-18 21:34:37 +0530

    landing page created

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/header/Header";
+import ReduxProvider from "@/provider";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +14,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ReduxProvider>
+          <Header />
+          {children}
+          <Footer/>
+        </ReduxProvider>
+      </body>
     </html>
   );
 }
