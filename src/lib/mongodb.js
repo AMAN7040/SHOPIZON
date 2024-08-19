03854bc2c236f48182dacd922b97d2ee6dd9aee3
AMAN7040 <amansingh79620@gmail.com>
2024-08-19 16:22:49 +0530

    bug fix

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,17 +1,13 @@
 import mongoose from 'mongoose';
 
 const connectMongo = async () => {
-    const uri = process.env.MONGODB_URI;
-    if (!uri) {
-      throw new Error('MONGODB_URI is not defined');
-    }
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  };
-  
+  const uri = process.env.MONGODB_URI;
+  if (!uri) {
+    throw new Error('MONGODB_URI is not defined');
+  }
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+  });
+};
 
 export default connectMongo;
-
-
