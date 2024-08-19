d79019add4ec68a629db7e45c33de74a7fa58cba
AMAN7040 <amansingh79620@gmail.com>
2024-08-19 15:22:11 +0530

    user store created

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
-import cartReducer from "./slice/cartSlice"
+import cartReducer from "./slice/cartSlice";
+import userReducer from "./slice/userSlice"
 const store = configureStore({
     reducer: {
-        cart: cartReducer
+        cart: cartReducer,
+        user: userReducer,
     }
 });
 
