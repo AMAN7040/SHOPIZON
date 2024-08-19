e7aaa5b433f91a71b2287f8a81cb249f0974f843
AMAN7040 <amansingh79620@gmail.com>
2024-08-19 16:13:52 +0530

    bug fix

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,12 +1,17 @@
 import mongoose from 'mongoose';
 
 const connectMongo = async () => {
-  if (mongoose.connections[0].readyState) return;
-
-  await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-};
+    const uri = process.env.MONGODB_URI;
+    if (!uri) {
+      throw new Error('MONGODB_URI is not defined');
+    }
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  };
+  
 
 export default connectMongo;
+
+
--- src/middleware.js
+++ src/middleware.js
@@ -1,20 +1,22 @@
-import { NextResponse } from 'next/server';
-import jwt from 'jsonwebtoken';
+import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
 
 export function middleware(request) {
-  const token = request.cookies.get('token') || request.headers.get('Authorization')?.split(' ')[1];
+  const token =
+    request.cookies.get("token") ||
+    request.headers.get("Authorization")?.split(" ")[1];
   if (token) {
     try {
       jwt.verify(token, process.env.JWT_SECRET);
       return NextResponse.next();
     } catch (error) {
-      return NextResponse.redirect('/signin');
+      return NextResponse.redirect("/signin");
     }
   }
 
-  return NextResponse.redirect('/signin');
+  return NextResponse.redirect("/signin");
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/profile/:path*'], // Apply to protected routes
+  matcher: ["/cart"], // Apply to the /car route
 };
