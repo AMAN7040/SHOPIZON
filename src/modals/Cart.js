8b138e9b32a1699f851f16e719f7e92eca6ab585
AMAN7040 <amansingh79620@gmail.com>
2024-08-22 20:13:30 +0530

    Cart model

---
+++ src/modals/Cart.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const CartItemSchema = new mongoose.Schema({
+  productId: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+});
+
+const CartSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true,
+  },
+  items: [CartItemSchema],
+});
+
+export default mongoose.models.Cart || mongoose.model("Cart", CartSchema);
