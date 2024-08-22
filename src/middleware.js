895068cc8661d931398993a8a6f0c1da7798da01
AMAN7040 <amansingh79620@gmail.com>
2024-08-22 23:56:26 +0530

    testing middleware issue

--- src/middleware.js
+++ src/middleware.js
@@ -2,24 +2,28 @@ import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 
 export async function middleware(request) {
+  console.log("Middleware executed");
   const token = request.cookies.get("token")?.value;
+  console.log("Token from request:", token);
 
   if (token) {
     try {
-      const decoded = jwtVerify(
+      const decoded = await jwtVerify(
         token,
         new TextEncoder().encode(process.env.JWT_SECRET)
       );
-
+      console.log("Token verified:", decoded);
       return NextResponse.next();
     } catch (error) {
+      console.error("Token verification failed:", error);
       return NextResponse.redirect(new URL("/signin", request.url));
     }
   }
 
+  console.log("No token found, redirecting to /signin");
   return NextResponse.redirect(new URL("/signin", request.url));
 }
 
 export const config = {
-  matcher: ["/cart"], //cart route protected
+  matcher: ["/cart"],
 };
